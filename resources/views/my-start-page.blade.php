3008401621488d0dee367ae3cbb9fa002a4f2a26
Sergey Dan <danilovserg1985s@gmail.com>
2025-03-08 11:04:13 +0000

    added dockerfile

---
+++ resources/views/my-start-page.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Моя страница</title>
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>Привет, это моя страница!</h1>
+    <p>Добро пожаловать на мой сайт.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyStartPageController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my-start-page', [MyStartPageController::class, 'show']);
