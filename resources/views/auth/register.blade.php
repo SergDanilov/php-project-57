d64b03d10e863ab1662120bd6491b9ede8b6b75b
Sergey Dan <danilovserg1985s@gmail.com>
2025-04-19 14:44:21 +0000

    added some libs, corrected forms frontend

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,11 +41,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Уже зарегистрированы?') }}
             </a>
 
-            <x-primary-button class="ms-4">
-                {{ __('Зарегистрироваться') }}
+            <x-primary-button class="bg-blue-500 hover:bg-blue-700 ms-4">
+                {{ __('Зарегистрировать') }}
             </x-primary-button>
         </div>
     </form>
