636175ff332b0c6b667d8e10c40f9927c0769795
Sergey Dan <danilovserg1985s@gmail.com>
2025-05-16 13:23:20 +0000

    created LadelSeeder

---
+++ database/seeders/LabelSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Label;
+use Illuminate\Database\Seeder;
+
+class LabelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $labels = [
+            ['name' => 'новая'],
+            ['name' => 'в работе'],
+            ['name' => 'тестирование'],
+            ['name' => 'в архиве'],
+            ['name' => 'срочно!'],
+        ];
+
+        foreach ($labels as $label) {
+            Label::firstOrCreate(
+                ['name' => $label['name']],
+                $label
+            );
+        }
+    }
+}
