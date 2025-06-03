2805fc5f098a7071cf2f6e791996383914b9199b
Sergey Dan <danilovserg1985s@gmail.com>
2025-06-03 13:28:13 +0000

    changed routes accordind policie's

--- routes/web.php
+++ routes/web.php
@@ -7,77 +7,56 @@ use App\Http\Controllers\LabelController;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Route;
 
+// Смена локали
 Route::get('/locale/{locale}', function ($locale) {
     if (!in_array($locale, ['en', 'ru'], true)) {
         abort(400);
     }
 
-    Cookie::queue('locale', $locale, 60 * 24 * 365); // 1 год
+    Cookie::queue('locale', $locale, 60 * 24 * 365);
     session()->put('locale', $locale);
 
     return redirect()->back();
 })->name('setlocale');
 
+// Главная страница (доступна всем)
+Route::get('/', function () {
+    return view('dashboard');
+})->name('dashboard');
+
 
 // Защищенные маршруты (только для авторизованных)
 Route::middleware(['auth', 'verified'])->group(function () {
-    // Dashboard
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
-    // Status CRUD (кроме index и show)
-    Route::resource('task_statuses', StatusController::class)
-        ->except(['index', 'show'])
-        ->names([
-            'create' => 'task_statuses.create',
-            'store' => 'task_statuses.store',
-            'edit' => 'task_statuses.edit',
-            'update' => 'task_statuses.update',
-            'destroy' => 'task_statuses.destroy',
-        ]);
-
-    // Task CRUD (кроме index и show)
-    Route::resource('tasks', TaskController::class)
-        ->except(['index', 'show'])
-        ->names([
-            'create' => 'tasks.create',
-            'store' => 'tasks.store',
-            'edit' => 'tasks.edit',
-            'update' => 'tasks.update',
-            'destroy' => 'tasks.destroy',
-        ]);
-
-    Route::resource('labels', LabelController::class)
-        ->except(['index', 'show'])
-        ->names([
-            'create' => 'labels.create',
-            'store' => 'labels.store',
-            'edit' => 'labels.edit',
-            'update' => 'labels.update',
-            'destroy' => 'labels.destroy',
-        ]);
-
-    // Профиль
+    // Профиль (оставляем middleware, так как это стандартный функционал Laravel)
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Ресурсные маршруты с автоматическими именами и защитой через Policy
+    Route::resource('task_statuses', StatusController::class)->only([
+        'create', 'store', 'edit', 'update', 'destroy'
+    ]);
+
+    Route::resource('tasks', TaskController::class)->only([
+        'create', 'store', 'edit', 'update', 'destroy'
+    ]);
+
+    Route::resource('labels', LabelController::class)->only([
+        'create', 'store', 'edit', 'update', 'destroy'
+    ]);
 });
 
-// Общие маршруты (доступны всем)
-// Главная страница
-Route::get('/', function () {
-        return view('dashboard');
-})->name('dashboard');
-
+// Публичные маршруты (чтение)
 Route::controller(StatusController::class)->group(function () {
     Route::get('/task_statuses', 'index')->name('task_statuses.index');
     Route::get('/task_statuses/{status}', 'show')->name('task_statuses.show');
 });
+
 Route::controller(TaskController::class)->group(function () {
     Route::get('/tasks', 'index')->name('tasks.index');
     Route::get('/tasks/{task}', 'show')->name('tasks.show');
 });
+
 Route::controller(LabelController::class)->group(function () {
     Route::get('/labels', 'index')->name('labels.index');
     Route::get('/labels/{label}', 'show')->name('labels.show');
